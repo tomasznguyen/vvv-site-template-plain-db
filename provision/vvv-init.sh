78c8a916e71b6ed0c0cb00867ff939d749c4a63f
Tomasz Nguyen <tomasznguyen@gmail.com>
2019-05-29 07:53:16 +0200

    Added provision script

---
+++ provision/vvv-init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Nginx Logs
+mkdir -p "${VVV_PATH_TO_SITE}/log"
+touch "${VVV_PATH_TO_SITE}/log/nginx-error.log"
+touch "${VVV_PATH_TO_SITE}/log/nginx-access.log"
+
+# Nginx configuration
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
